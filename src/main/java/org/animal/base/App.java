7aaf74e31039af549de49ac26f9237e3ebe91450
Bala <labarefo2015@gmail.com>
2023-10-06 12:07:19 +0200

    update

--- src/main/java/org/animal/base/App.java
+++ src/main/java/org/animal/base/App.java
@@ -6,67 +6,37 @@ package org.animal.base;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("---------------------- poisson -------------------");
-        System.out.println(Animal.presenterAnimal("poisson"));
-        System.out.println(Animal.deplacement("poisson"));
-        System.out.println(Animal.cri("poisson"));
+        System.out.println("---------------------- chat -------------------");
+        System.out.println(Animal.presenterAnimal("chat"));
+        System.out.println(Animal.deplacement("chat"));
+        System.out.println(Animal.cri("chat"));
     }
 
     static class Animal {
 
         public static String cri(String animal) {
             return switch (animal) {
-                case "chien" -> """
-                        ==>j'aboie
-                        ==>waf waf !!!
-                        """;
                 case "chat" -> """
                         ==>je miaule
                         ==>miaou miaou !!!
                         """;
-                case "mouton" -> """
-                        ==>je bêle
-                        ==>mbêêêêêê !!!!!
-                        """;
-                case "coq" -> """
-                        ==>je chante
-                        ==>cocoriricôôô !!!
-                        """;
-                case "poisson" -> "==>je ne sais pas crier !!!";
                 default -> "Animal inconnu";
             };
         }
 
         public static String deplacement(String animal) {
             return switch (animal) {
-                case "chien", "chat", "mouton" -> "==>je marche à 4 pates sur la terre ferme";
-                case "coq" -> "==>je marche à 2 pates sur la terre ferme";
-                case "poisson" -> "==>je nage dans l'eau";
+                case "chat" -> "==>je marche à 4 pates sur la terre ferme";
                 default -> "Animal inconnu";
             };
         }
 
         public static String presenterAnimal(String animal) {
             return switch (animal) {
-                case "chien", "chat" ->
+                case "chat" ->
                         """
                         ==>je suis un animal domestique
                         ==>je suis carnivore
-                        """;
-                case "mouton" ->
-                        """
-                        ==>je suis un animal domestique
-                        ==>je suis herbivore
-                        """;
-                case "coq" ->
-                        """
-                        ==>je suis un animal domestique
-                        ==>je me nourris de graines
-                        """;
-                case "poisson" ->
-                        """
-                        ==>je suis un animal qui vit dans l'eau
-                        ==>je me nourris de plactons
                         """;
                 default -> "Animal inconnu";
             };
